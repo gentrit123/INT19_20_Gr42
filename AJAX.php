9886036bbd6b3bc7bec14329143e2f76bc5158c9
Fisnik369 <59198126+Fisnik369@users.noreply.github.com>
2020-05-17 01:28:00 +0200

    Create AJAX.php

---
+++ AJAX.php
@@ -0,0 +1,46 @@
+<?php 
+
+ 
+require_once('init.php');
+require_once('query_auth.php');
+
+
+
+if(isset($_POST['do_login'])){
+
+    $username = $_POST['username'];
+    $pass = $_POST['password'];
+
+   if(user_login($username,$pass)){
+       
+       $_SESSION['username'] = $username;
+       
+       echo 1;
+       
+       if(isset($_POST['username']) && !empty($_POST['username'])){
+
+        $cookie_value=$_POST['username'];
+        setcookie('username',$cookie_value, time() + 100000000 );
+        }
+        else{
+            $cookie_value=$_POST['username'];
+            setcookie('username', " ", time() + 860000000);
+
+        }
+
+
+    }
+   else{
+       echo 2;
+   }
+    exit();
+}
+
+
+
+
+
+
+ 
+
+?>
