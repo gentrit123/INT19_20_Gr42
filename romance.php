22011bf8c00ab61eb6fcf2682cf56276880f7026
Genti Krasniqi <59203667+gentikk@users.noreply.github.com>
2020-05-17 11:41:58 -0700

    Update romance.php

--- romance.php
+++ romance.php
@@ -7,3 +7,11 @@
         box-sizing:border-box;
       }
       body{
+        margin:0;
+        padding:0;
+        font-family:'Georgia',sans-serif;
+        background:#d4d4d4;
+        overflow-x:hidden;
+        background-color:black;
+        background-repeat:repeat;
+      }
